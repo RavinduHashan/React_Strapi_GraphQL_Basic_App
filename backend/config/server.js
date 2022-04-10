f0759455336de48a2e63c439eb3532d7ec23fec8
RavinduHashan <ravinduhashan78@gmail.com>
2022-04-10 10:01:37 +0530

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '801ed57ce72fb01f307085b8277ec13f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecdf31df-0c04-4794-8278-023e0aeaf2a3'
+};
